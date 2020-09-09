987c68792574e908cc86934c59cd113fbb60a75e
loco <loco@list.ru>
2020-09-09 13:09:22 +0300

    Added guzzlehttp/psr7

--- public/index.php
+++ public/index.php
@@ -1,6 +1,32 @@
 <?php
 
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-echo "phoneBook";
+### Initialization
+$request = ServerRequest::fromGlobals();
+
+### Action
+$name = $request->getQueryParams()['name'] ?: 'Guest';
+
+### Response
+
+/** @var Response $response */
+$response = (new Response(200, [],'Hello, ' . $name . '!'))
+    ->withHeader('X-Developer', 'loco');
+
+header(sprintf(
+    'HTTP/%s %d %s',
+    $response->getProtocolVersion(),
+    $response->getStatusCode(),
+    $response->getReasonPhrase()
+));
+
+foreach ($response->getHeaders() as $name => $values) {
+    header($name . ':' . implode(', ', $values));
+}
+
+echo $response->getBody();
